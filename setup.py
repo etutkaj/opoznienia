4d45bd4829e2064612709cea0f0f279dcd6f0776
etutkaj <95316070+etutkaj@users.noreply.github.com>
2022-01-30 22:35:24 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='opoznienia',
+    url='https://github.com/etutkaj/opoznienia',
+    author='Edyta Tutkaj',
+    author_email='263325@student.pwr.edu.pl',
+    packages=['opoznienia'],
+    install_requires=['pandas'],
+    version='0.1',
+    license='MIT',
+    description='opoznienia komunikacji',
+)
